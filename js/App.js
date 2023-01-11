60b5ca0e5c16b8b2af1702aef29e7b7e0449d6fb
MaryAnzh <maryang@mail.ru>
2023-01-11 18:59:16 +0300

    feat: add base footer layout and style

--- js/App.js
+++ js/App.js
@@ -1,46 +1,52 @@
 import { Header } from './components/common/header/Header.js';
 import { About } from './components/section/about/About.js';
 import { Contacts } from './components/section/contacts/Contacts.js';
+import { Footer } from './components/common/footer/Footer.js';
 
 export class App {
     header;
     main;
-    sectionList;
+    footer;
+
     sectionMap;
+    sectionList;
     currentSectionName;
-    currentLoadSection;
+    currentSectionComponent;
 
     constructor() {
         this.header = document.querySelector('.header');
-        this.main = document.querySelector('.main');
-        this.isSectionOnLoad = false;
-        this.sectionList = [];
+        this.main = document.querySelector('.main__wrapper');
+        this.footer = document.querySelector('.footer');
+        
         this.sectionMap = new Map([
             ['about', About],
             ['contacts', Contacts],
         ]);
+        this.sectionList = [];
         this.sectionMap.forEach((el, key) => this.sectionList.push(key));
         this.currentSectionName = this.sectionList[0];
-        this.currentLoadSection = null;
+        this.currentSectionComponent = null;
     }
 
     drawApp() {
         const headerFill = new Header(this.changeSection, this.sectionList);
         this.header.append(headerFill.header.node);
+        const footerFill = new Footer();
+        this.footer.append(footerFill.node);
+
         this.drawSection(this.currentSectionName);
     }
 
     drawSection(sectionName) {
-        if (this.currentLoadSection !== null) {
-            this.currentLoadSection.destroy();
+        if (this.currentSectionComponent !== null) {
+            this.currentSectionComponent.destroy();
         }
 
         const component = this.sectionMap.get(sectionName);
         const section = new component();
-        this.currentLoadSection = section;
+        this.currentSectionComponent = section;
         this.currentSectionName = sectionName;
         
-        this.main.innerHTML = '';
         this.main.append(section.section.node);
     }
 
