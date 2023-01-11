7c46d04a997e345d21bb55f1199b90891eda2c4a
MaryAnzh <maryang@mail.ru>
2023-01-11 21:41:49 +0300

    feat: add base CV components nav style

--- js/components/common/header/Header.js
+++ js/components/common/header/Header.js
@@ -3,19 +3,32 @@ import { Component } from "../../../util/component.js";
 export class Header extends Component {
     nav;
     navList;
+    changeSection;
 
     constructor(changeSection, sectionList) {
         super('div', 'header-wrap', '');
+        this.changeSection = changeSection;
         this.nav = new Component('nav', 'header-wrap__nav', '');
         this.navList = [];
-        sectionList.forEach(name => {
+        sectionList.forEach((name, i) => {
+
             const li = new Component('li', 'header-wrap__nav__item', name);
-            li.node.onclick = () => changeSection(name);
+            li.node.onclick = (e) => this.onclick(e, name);
             this.navList.push(li.node);
+            if (i === 0) {
+                li.node.classList.add('active-nav-item');
+            }
             this.nav.node.append(li.node);
         });
 
         this.node.append(this.nav.node);
+    }
+
+    onclick = (e, name) => {
+        const li = e.target;
+        this.navList.forEach(el => el.classList.remove('active-nav-item'));
+        li.classList.add('active-nav-item');
+        this.changeSection(name);
     }
 
     destroy() {
