dc999013a4d079e5aab4a6234969cf85fbb7beaf
MaryAnzh <maryang@mail.ru>
2023-01-11 18:00:16 +0300

    feat: add base navigation

--- js/App.js
+++ js/App.js
@@ -5,33 +5,46 @@ import { Contacts } from './components/section/contacts/Contacts.js';
 export class App {
     header;
     main;
-    section;
+    sectionList;
     sectionMap;
-    aboutSection;
-    contactsSection;
+    currentSectionName;
+    currentLoadSection;
 
     constructor() {
         this.header = document.querySelector('.header');
         this.main = document.querySelector('.main');
-        this.aboutSection = new About();
-        this.contactsSection = new Contacts();
+        this.isSectionOnLoad = false;
+        this.sectionList = [];
         this.sectionMap = new Map([
-            ['about', this.aboutSection],
-            ['contacts', this.contactsSection],
+            ['about', About],
+            ['contacts', Contacts],
         ]);
-
+        this.sectionMap.forEach((el, key) => this.sectionList.push(key));
+        this.currentSectionName = this.sectionList[0];
+        this.currentLoadSection = null;
     }
 
     drawApp() {
-        const headerFill = new Header();
+        const headerFill = new Header(this.changeSection, this.sectionList);
         this.header.append(headerFill.header.node);
+        this.drawSection(this.currentSectionName);
     }
 
-    drawSection() {
+    drawSection(sectionName) {
+        if (this.currentLoadSection !== null) {
+            this.currentLoadSection.destroy();
+        }
 
+        const component = this.sectionMap.get(sectionName);
+        const section = new component();
+        this.currentLoadSection = section;
+        this.currentSectionName = sectionName;
+        
+        this.main.innerHTML = '';
+        this.main.append(section.section.node);
     }
 
-    changeSectionOnClick = () => {
-
+    changeSection = (sectionName) => {
+        this.drawSection(sectionName);
     }
 }
--- js/components/common/header/Header.js
+++ js/components/common/header/Header.js
@@ -2,9 +2,25 @@ import { Component } from "../../../util/component.js";
 
 export class Header {
     header;
+    nav;
+    navList;
 
-    constructor() {
+    constructor(changeSection, sectionList) {
         this.header = new Component('div', 'header-wrap', '');
+        this.nav = new Component('nav', 'header-wrap__nav', '');
+        this.header.node.append(this.nav.node);
+        this.navList = [];
+        sectionList.forEach(name => {
+            const li = new Component('li', 'header-wrap__nav__item', name);
+            li.node.onclick = () => changeSection(name);
+            this.navList.push(li.node);
+            this.nav.node.append(li.node);
+        });
+    }
+
+    destroy() {
+        this.header.destroy();
+        this.navList.forEach(el => el.onclick = null);
     }
 
 }
