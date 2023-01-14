f602b1e403b0a5c5b0e47eddcc64156191584f3a
MaryAnzh <maryang@mail.ru>
2023-01-14 20:51:18 +0300

    feat: add hight Education info

--- js/components/section/education/Education.js
+++ js/components/section/education/Education.js
@@ -5,6 +5,27 @@ export class Education extends Component {
     info;
     hightEducation;
     curse;
+    universityInfo = {
+        specialty: 'Web Design and Computer Graphics',
+        qualification: 'Developer-web designer',
+        graduation: '05/20021',
+    };
+    curseInfo = [
+        {
+            title: 'JavaScript/Front-end2021Q3',
+            certificate: true,
+            date: '03/2022',
+            position: 201,
+            score: 3920
+        },
+        {
+            title: 'Angular 2022Q1',
+            certificate: true,
+            date: '06/2022',
+            position: 80,
+            score: 1031
+        },
+    ];
 
     constructor() {
         super('section', 'education');
@@ -17,18 +38,38 @@ export class Education extends Component {
         const hightEducationInfo = new Component('div', 'education__info__hight__info');
         const hightTitle = new Component('h4', 'education__info__hight__info__title ', 'Hight Education');
         const university = new Component('h6', 'education__info__hight__info__university', 'Business of BSU');
-        const universityInfo = {
-            specialty: 'Web Design and Computer Graphics',
-            qualification: 'Developer-web designer',
-            graduation: '05/20021',
-        };
+
         hightEducationInfo.node.append(hightTitle.node, university.node);
 
-        for (const key in universityInfo) {
+        for (const key in this.universityInfo) {
             const title = new Component('h6', `education__info__hight__info__name`, key);
-            const info = new Component('p', `education__info__hight__info__value`, universityInfo[key]);
+            const info = new Component('p', `education__info__hight__info__value`, this.universityInfo[key]);
             hightEducationInfo.node.append(title.node, info.node);
         }
+
+        const curseTitle = new Component('h2', 'education__info__curses__title', 'Curses');
+        const curseSubTitle = new Component('h3', 'education__info__curses__sub-title', 'RS School');
+        const cursesWrap = new Component('div', 'education__info__curses__wrap');
+
+        this.curse.node.append(curseTitle.node, curseSubTitle.node, cursesWrap.node);
+
+        this.curseInfo.forEach(group => {
+            const valueClass = 'education__info__curses__wrap__curse-wrap__value';
+            const wrap = new Component('div', 'education__info__curses__wrap__curse-wrap');
+            const title = new Component('h4', valueClass, group.title);
+            const certificate = new Component('div', valueClass);
+            const text = new Component('span', 'education__info__curses__wrap__curse-wrap__value__certificate', 'certificate');
+            const icon = new Component('div', 'education__info__curses__wrap__curse-wrap__check-icon');
+            certificate.node.append(text.node, icon.node);
+
+            const date = new Component('p', valueClass, group.date);
+            const position = new Component('p', valueClass, `position: ${group.position}`);
+            const score = new Component('p', valueClass, `score: ${group.score}`);
+
+            wrap.node.append(title.node, certificate.node, date.node, position.node, score.node);
+            cursesWrap.node.append(wrap.node);
+        });
+
 
         hightEducationImgWrap.node.append(hightEducationImg.node);
         this.hightEducation.node.append(hightEducationImgWrap.node, hightEducationInfo.node);
